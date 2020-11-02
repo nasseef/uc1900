22f66154552cfc0383692f966363463bebe2f8f7
Nasseef Abukamail <abukamai@ohio.edu>
2020-11-02 17:19:14 -0500

    Change hello to goodbye

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
 # this is a comments
 def main():
-    print("Hello World")
-    s = "hello"
+    print("Goodbye World")
+    s = "goodbye"
     s = s.capitalize()
     print(s)
 if __name__ == "__main__":
